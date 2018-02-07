384b67cc2bd9c6ec4919cc166ef9a544c7c823d0
Amanda Phillips <amanda@phillipsdev.org>
2018-02-07 17:20:42 -0600

    add dummy-data

---
+++ src/dummy-data.js
@@ -0,0 +1,62 @@
+const dummyData = [
+    {
+        firstName:  "Chaddie",
+        lastName:  "Veldstra",    
+        jobTitle:  "Mechanical Systems Engineer",   
+        connections:  66
+    },
+    {
+        firstName: "Dominica",
+        lastName: "McFaell",
+        jobTitle: "Operator",
+        connections: 613
+    },
+    {
+        firstName: "Ezri",
+        lastName: "Akeherst",
+        jobTitle: "Cost Accountant",
+        connections: 470
+    },
+    {
+        firstName: "Percy",
+        lastName: "Le Huquet",
+        jobTitle: "Software Test Engineer I",
+        connections: 574
+    },
+    {
+        firstName: "Kristoforo",
+        lastName: "Orring",
+        jobTitle: "Web Designer IV",
+        connections: 7
+    },
+    {
+        firstName: "Ruprecht",
+        lastName: "Chantler",
+        jobTitle: "Registered Nurse",
+        connections: 93
+    },
+    {
+        firstName: "Terri",
+        lastName: "Devaney",
+        jobTitle: "Project Manager",
+        connections: 1
+    },
+    {
+        firstName: "Celia",
+        lastName: "Ramirez",
+        jobTitle: "Analyst Programmer",
+        connections: 4
+    },
+    {
+        firstName: "Clair",
+        lastName: "Pacey",
+        jobTitle: "Chemical Engineer",
+        connections: 31
+    },
+    {
+        firstName: "Jase",
+        lastName: "Dawidowsky",
+        jobTitle: "Dental Hygienist",
+        connections: 830
+    },
+]
